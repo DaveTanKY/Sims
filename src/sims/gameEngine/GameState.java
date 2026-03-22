80244224945118a098b0b550a8b1229cc937cd74
Dave <dave21sg@gmail.com>
2026-03-22 18:06:36 +0800

    modified actionmenu to use mapping.
    added several methods to display options depending on sim in location and if sim is friends with another.
    
    modified method to allow sims to visit friends home if they are at home

--- src/sims/gameEngine/GameState.java
+++ src/sims/gameEngine/GameState.java
@@ -2,6 +2,7 @@ package sims.gameEngine;
 
 import sims.actions.Activity;
 import sims.career.Career;
+import sims.entity.Relationship;
 import sims.entity.Sim;
 import sims.needs.need;
 import sims.world.*;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.stream.Collectors;
 
 public class GameState {
 
@@ -51,6 +53,7 @@ public class GameState {
 
         OutsideLocation work = new OutsideLocation("Office", "Title", "Developer");
         Activity workActivity = new Activity("Work", (8 * 60), "Salary", 0);
+
         work.addActivity(workActivity);
         addLocation(work);
     }
@@ -100,6 +103,8 @@ public class GameState {
                 showMoveMenu();
                 break;
             case 5:
+                break;
+            case 6:
                 endGame();
                 break;
         }
@@ -121,7 +126,7 @@ public class GameState {
                 gameState = 2;
                 break;
             case 3:
-                gameState = 5;
+                gameState = 6;
                 break;
         }
     }
@@ -166,8 +171,11 @@ public class GameState {
     }
 
     public void showActionMenu() {
+        // Display location info
         if (currentSim.getLocation() instanceof HomeLocation) {
-            System.out.println("\nYour current location is " + ((HomeLocation) currentSim.getLocation()).getHome().getName() + " : " + currentSim.getLocation().getName());
+            System.out.println("\nYour current location is " +
+                    ((HomeLocation) currentSim.getLocation()).getHome().getName() +
+                    " : " + currentSim.getLocation().getName());
         } else {
             System.out.println("\nYour current location is " + currentSim.getLocation().getName());
         }
@@ -175,10 +183,21 @@ public class GameState {
         showTime();
         showStats(currentSim);
 
-        System.out.println("\n-------------Please choose action for SIM--------------");
-        System.out.println("\n[1] Move Location");
+        // Sims in same location
+        List<Sim> simLocList = currentSim.getLocation().getLocSimList()
+                .stream()
+                .filter(sim -> !sim.equals(currentSim))
+                .collect(Collectors.toList());
+
+        if (!simLocList.isEmpty()) {
+            System.out.println("\n-------------Sims in the same location--------------");
+            simLocList.forEach(sim -> System.out.println(sim.getName()));
+        }
+
+        // Build activity and upgrade lists
         List<Activity> activityList = new ArrayList<>(currentSim.getLocation().getActivity());
         List<HomeUpgrade> upgradeOption = new ArrayList<>();
+
         if (currentSim.getLocation() instanceof HomeLocation) {
             List<HomeUpgrade> upgradeList = ((HomeLocation) currentSim.getLocation()).getUpgradeList();
             for (HomeUpgrade upgrade : upgradeList) {
@@ -189,72 +208,285 @@ public class GameState {
                 }
             }
         }
+
+        // Print menu
+        System.out.println("\n-------------Please choose action for SIM--------------");
+
+        Map<Integer, Runnable> actions = new HashMap<>();
+        int optionIndex = 1;
+        System.out.println("\n[" + optionIndex + "] Move Location");
+        actions.put(optionIndex++, () -> gameState = 4);
+
+
+/*
         for (int i = 0; i < activityList.size(); i++) {
             Activity activity = activityList.get(i);
-            if(activity.getImpactedNeed() == "Salary")
-            {
-                System.out.println("[" + (i + 2) + "] " + activity.getName() + " - " + activity.getImpactedNeed() + " : " + getStringTime(activity.getDuration()) + " : $" + (currentSim.getCareer().getSalary() + currentSim.getCareer().getBonus()) +   " + ( Career Bonus : " + currentSim.getCareer().getLevel() + "% )");
-            }
-            else
-            {
-                System.out.println("[" + (i + 2) + "] " + activity.getName() + " - " + activity.getImpactedNeed() + " :  " + getStringTime(activity.getDuration()) + " - " + activity.getValue() + " Stats");
+            if ("Salary".equals(activity.getImpactedNeed())) {
+                System.out.println("[" + (i + 2) + "] " + activity.getName() +
+                        " - " + activity.getImpactedNeed() + " : " +
+                        getStringTime(activity.getDuration()) + " : $" +
+                        (currentSim.getCareer().getSalary() + currentSim.getCareer().getBonus()) +
+                        " + ( Career Bonus : " + currentSim.getCareer().getLevel() + "% )");
+            } else {
+                System.out.println("[" + (i + 2) + "] " + activity.getName() +
+                        " - " + activity.getImpactedNeed() + " : " +
+                        getStringTime(activity.getDuration()) + " - " +
+                        activity.getValue() + " Stats");
             }
         }
-        int count = activityList.size() + 2;
-        if (!upgradeOption.isEmpty()) {
+
+ */
+
+        for(Activity activity : activityList)
+        {
+            System.out.println("[" + optionIndex + "] " + activity.getName() + " - " + activity.getImpactedNeed());
+            int idx = optionIndex++;
+            actions.put(idx, ()-> {
+                if(currentSim.getBank() >= activity.getCost()) {
+                    activity.performActivity(currentSim);
+                    decayLoop(activity.getDuration(), activity.getImpactedNeed());
+                }
+                else
+                {
+                    System.out.println("Insufficient Funds!");
+                }
+            });
+        }
+
+
+        if (!upgradeOption.isEmpty() && currentSim.getHome() == ((HomeLocation) currentSim.getLocation()).getHome()) {
             System.out.println("\n----------Purchase Upgrades to unlock activities!----------");
             for (HomeUpgrade option : upgradeOption) {
-                System.out.println("[" + (count) + "] " + "Purchase " + option.getName() + " ( $" + option.getPrice() + " ) " + " to unlock \n- " + option.getActivity().getName() + " : " + option.getActivity().getImpactedNeed() + " + " + option.getActivity().getValue() + " Stats");
-                count += 1;
+                System.out.println("[" + optionIndex + "] Purchase " + option.getName() +
+                        " ( $" + option.getPrice() + " ) to unlock \n- " +
+                        option.getActivity().getName() + " : " +
+                        option.getActivity().getImpactedNeed() + " + " +
+                        option.getActivity().getValue() + " Stats");
+                optionIndex++;
             }
         }
 
-        System.out.println("[" + (activityList.size()+upgradeOption.size()+2) + "] Exit to main menu");
-        int choice = readInt("\nInput : ", activityList.size() + upgradeOption.size() + 2);
 
-        if(choice == 1)
+        if(!simLocList.isEmpty())
         {
-            gameState = 4;
+            System.out.println("[" + optionIndex + "] Interact with other Sims");
+            actions.put(optionIndex, () -> {
+                System.out.println("\nChoose a Sim to interact with:");
+                for (int i = 0; i < simLocList.size(); i++) {
+                    System.out.println("[" + (i + 1) + "] " + simLocList.get(i).getName());
+                }
+                int simChoice = readInt("Select Sim: ", simLocList.size());
+                Sim chosenSim = simLocList.get(simChoice - 1);
+                // Example interaction logic
+
+                int optionList = 1;
+                System.out.println("[1] Talk to " + chosenSim.getName());
+                if(!currentSim.getRelationshipMap().containsKey(chosenSim.getUUID()))
+                {
+                    System.out.println("[2] Add " + chosenSim.getName() + " as friend");
+                    optionList ++;
+                }
+                int interactChoice = readInt("Select choice: ", optionList);
+                switch (interactChoice)
+                {
+                    case 1:
+                        Activity interactActivity = new Activity("Talk" , 10, "Social", 30);
+                        interactActivity.performActivity(currentSim);
+                        interactActivity.performActivity(chosenSim);
+                        decayLoop(interactActivity.getDuration(), interactActivity.getImpactedNeed());
+                        break;
+                    case 2:
+                        Relationship newRelationship = new Relationship(currentSim, chosenSim);
+                        currentSim.addRelationship(chosenSim.getUUID(), newRelationship);
+                        chosenSim.addRelationship(currentSim.getUUID(), newRelationship);
+                        break;
+                }
+
+                // You could expand this with friendship, dialogue, or stat changes
+            });
+            optionIndex ++;
         }
-        else if(choice <= activityList.size() + 1)
+
+
+        if(!currentSim.getRelationshipMap().isEmpty())
         {
-            //execute activity list action
-            Activity selectedActivity = activityList.get(choice - 2);
+            System.out.println("[" + optionIndex + "] Interact with friends");
+            actions.put(optionIndex, () -> {
+                System.out.println("\nChoose a Friend to interact with:");
+                List<Integer> friendIds = new ArrayList<>(currentSim.getRelationshipMap().keySet());
+                for (int i = 0; i < friendIds.size(); i++) {
+                    Relationship rel = currentSim.getRelationshipMap().get(friendIds.get(i));
+                    Sim friend = rel.getOtherSim(currentSim); // helper method to get the other Sim
+                    System.out.println("[" + (i + 1) + "] " + friend.getName());
+                }
+                int friendChoice = readInt("Select Friend: ", friendIds.size());
+                Relationship chosenRel = currentSim.getRelationshipMap().get(friendIds.get(friendChoice - 1));
+                Sim friend = chosenRel.getOtherSim(currentSim);
 
-            if(currentSim.getBank() >= selectedActivity.getCost())
-            {
-                selectedActivity.performActivity(currentSim);
-                decayLoop(selectedActivity.getDuration(), selectedActivity.getImpactedNeed());
-            }
-            else
-            {
-                System.out.println("Insufficient Funds!");
-            }
+
+
+                // Example: increase friendship level
+
+                int optionList = 1;
+                System.out.println("[1] Message " + friend.getName());
+                if(friend.getHome().getHomeLocation().contains(friend.getLocation()))
+                {
+                    System.out.println("[2] Visit " + friend.getHome().getName());
+                    optionList ++;
+                }
+                int friendActivity = readInt("Input : ", optionList);
+                switch (friendActivity)
+                {
+                    case 1:
+                        Activity messageFriend = new Activity("Message Friend", 10, "Social", 20);
+                        messageFriend.performActivity(currentSim);
+                        messageFriend.performActivity(friend);
+                        decayLoop(messageFriend.getDuration(), messageFriend.getImpactedNeed());
+                        chosenRel.increaseFriendship(20);
+                        break;
+                    case 2:
+                        friend.getHome().moveTo(currentSim);
+                        break;
+                }
+
+            });
+            optionIndex++;
 
         }
-        else if(choice <= activityList.size() + upgradeOption.size() + 1)
-        {
-            //execute purchasing of upgrade
-            HomeUpgrade selectedUpgrade = upgradeOption.get(choice - activityList.size()-2);
-            if(currentSim.getBank() >= selectedUpgrade.getPrice())
-            {
-                System.out.println(currentSim.getBank());
-                currentSim.updateBank(selectedUpgrade.getPrice());
-                System.out.println("After : " + currentSim.getBank());
-                selectedUpgrade.purchaseUpgrade();
+
+        System.out.println("[" + optionIndex + "] Exit to main menu");
+
+        // Build actions map
+        for (int i = 0; i < activityList.size(); i++) {
+            int index = i + 2;
+            Activity activity = activityList.get(i);
+            actions.put(index, () -> {
+                if (currentSim.getBank() >= activity.getCost()) {
+                    activity.performActivity(currentSim);
+                    decayLoop(activity.getDuration(), activity.getImpactedNeed());
+                } else {
+                    System.out.println("Insufficient Funds!");
+                }
+            });
+        }
+        int upgradeStart = activityList.size() + 2;
+        if (!upgradeOption.isEmpty() && currentSim.getHome() == ((HomeLocation) currentSim.getLocation()).getHome()) {
+
+            for (int i = 0; i < upgradeOption.size(); i++) {
+                int index = upgradeStart + i;
+                HomeUpgrade upgrade = upgradeOption.get(i);
+                actions.put(index, () -> {
+                    if (currentSim.getBank() >= upgrade.getPrice()) {
+                        currentSim.updateBank(upgrade.getPrice());
+                        upgrade.purchaseUpgrade();
+                    } else {
+                        System.out.println("Insufficient Funds!");
+                    }
+                });
             }
-            else
-            {
-                System.out.println("Insufficient Funds!");
-            }
+        }
 
 
+        /*
+        if (!simLocList.isEmpty()) {
+            actions.put(optionIndex++, () -> {
+                System.out.println("\nChoose a Sim to interact with:");
+                for (int i = 0; i < simLocList.size(); i++) {
+                    System.out.println("[" + (i + 1) + "] " + simLocList.get(i).getName());
+                }
+                int simChoice = readInt("Select Sim: ", simLocList.size());
+                Sim chosenSim = simLocList.get(simChoice - 1);
+                // Example interaction logic
+
+                int optionList = 1;
+                System.out.println("[1] Talk to " + chosenSim.getName());
+                if(!currentSim.getRelationshipMap().containsKey(chosenSim.getUUID()))
+                {
+                    System.out.println("[2] Add " + chosenSim.getName() + " as friend");
+                    optionList ++;
+                }
+
+
+                int interactChoice = readInt("Select choice: ", optionList);
+                switch (interactChoice)
+                {
+                    case 1:
+                        Activity interactActivity = new Activity("Talk" , 10, "Social", 30);
+                        interactActivity.performActivity(currentSim);
+                        interactActivity.performActivity(chosenSim);
+                        decayLoop(interactActivity.getDuration(), interactActivity.getImpactedNeed());
+                        break;
+                    case 2:
+                        Relationship newRelationship = new Relationship(currentSim, chosenSim);
+                        currentSim.addRelationship(chosenSim.getUUID(), newRelationship);
+                        chosenSim.addRelationship(currentSim.getUUID(), newRelationship);
+                        break;
+                }
+
+                // You could expand this with friendship, dialogue, or stat changes
+            });
+            optionIndex++;
         }
-        else
-        {
-            gameState = 0;
+
+         */
+
+
+        /*
+        // Interact with Friends
+        if (!currentSim.getRelationshipMap().isEmpty()) {
+            actions.put(optionIndex++, () -> {
+                System.out.println("\nChoose a Friend to interact with:");
+                List<Integer> friendIds = new ArrayList<>(currentSim.getRelationshipMap().keySet());
+                for (int i = 0; i < friendIds.size(); i++) {
+                    Relationship rel = currentSim.getRelationshipMap().get(friendIds.get(i));
+                    Sim friend = rel.getOtherSim(currentSim); // helper method to get the other Sim
+                    System.out.println("[" + (i + 1) + "] " + friend.getName());
+                }
+                int friendChoice = readInt("Select Friend: ", friendIds.size());
+                Relationship chosenRel = currentSim.getRelationshipMap().get(friendIds.get(friendChoice - 1));
+                Sim friend = chosenRel.getOtherSim(currentSim);
+
+
+
+                // Example: increase friendship level
+
+                int optionList = 1;
+                System.out.println("[1] Message " + friend.getName());
+                if(friend.getHome().getHomeLocation().contains(friend.getLocation()))
+                {
+                    System.out.println("[2] Visit " + friend.getHome().getName());
+                    optionList ++;
+                }
+                int friendActivity = readInt("Input : ", optionList);
+                switch (friendActivity)
+                {
+                    case 1:
+                        Activity messageFriend = new Activity("Message Friend", 10, "Social", 20);
+                        messageFriend.performActivity(currentSim);
+                        messageFriend.performActivity(friend);
+                        decayLoop(messageFriend.getDuration(), messageFriend.getImpactedNeed());
+                        chosenRel.increaseFriendship(20);
+                        break;
+                    case 2:
+                        friend.getHome().moveTo(currentSim);
+                        break;
+                }
+
+            });
+            optionIndex++;
         }
+
+         */
+
+
+        actions.put(optionIndex, () -> gameState = 0); // Exit
+        // Execute choice
+        int choice = readInt("\nInput : ", optionIndex);
+        actions.getOrDefault(choice, () -> System.out.println("Invalid choice." )).run();
     }
+
+
+
 
     public void showMoveMenu()
     {
