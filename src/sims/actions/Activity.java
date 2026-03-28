0a1d0530ab40b699d6d89e96cbea3e1e28f35d39
Dave <dave21sg@gmail.com>
2026-03-28 17:16:54 +0800

    modified performActivity method to add exp for skill or career

--- src/sims/actions/Activity.java
+++ src/sims/actions/Activity.java
@@ -3,6 +3,7 @@ package sims.actions;
 import sims.entity.Sim;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents an activity that a Sim can perform in the Sims world.
@@ -100,7 +101,20 @@ public class Activity {
      * @param sim the Sim performing the activity
      */
     public void performActivity(Sim sim) {
-        sim.updateNeeds(impactedNeed, value);
+        if(impactedNeed != "Salary") {
+            Map<String, SkillManager> skillmap = sim.getSkillMap();
+            SkillManager skill = skillmap.get(impactedNeed);
+            skill.earnXP();
+            sim.updateNeeds(impactedNeed,  (value + (skill.getLevel() * 5)));
+        }
+        else
+        {
+            System.out.println("Bank previous : " + sim.getBank());
+            sim.addBank(sim.getCareer().getSalary()+sim.getCareer().getBonus());
+            sim.getCareer().earnXP();
+            System.out.println("Bank after : " + sim.getBank());
+        }
+
     }
 
 
